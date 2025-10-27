f8fc26f4e79076d4e882dad4f77c3d9ae36c29e1
Cleverconnection <contato@cleverconnection.com.br>
2025-10-27 02:23:11 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import FinanceCRM from './FinanceCRM';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FinanceCRM />
+  </React.StrictMode>
+);
